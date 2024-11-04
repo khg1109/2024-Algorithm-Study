a477e91aca22dafe3c867ac73a3c44bb6208e629
gupaejwa <sin010423@naver.com>
2024-11-04 20:03:00 +0900

    [신승우] 콰트로치즈피자.py

--- [신승우] 콰트로치즈피자.py
+++ [신승우] 콰트로치즈피자.py
@@ -1,17 +1,15 @@
+import sys
+
 cnt = 0
-#print('토핑갯수를 입력하세요')
-topping_n = int(input())    #피자에 넣을 토핑 개수
-pizza = input().split()   # 공백을 두고 토핑 입력
-pizza = pizza[:topping_n]     # 토핑 갯수만큼만 입력을 받음
-pizza = set(pizza)  # 세트를 통해 중복을 제거
+topping_no = int(sys.stdin.readline().strip())    # 피자에 넣을 토핑 개수 입력
+pizzaSet = sys.stdin.readline().strip().split()   # 공백을 두고 토핑 입력
+pizzaSet = set(pizzaSet[:topping_no])             # 토핑 갯수만큼만 생성하고 세트형으로 중복된 문자열 제거
 
-for topping in pizza:
-    if topping.endswith('Cheese'):
-        cnt += 1    # 'Cheese'가 포함된 문자열을 발견할 때마다 카운트
+for topping in pizzaSet:
+    if topping.endswith('Cheese'):                # pizzaSet 안의 요소의 문자열 끝에 'Cheese'가 있는지 판별
+        cnt += 1                                  # 'Cheese'가 포함된 문자열을 발견할 때마다 카운트
 
-if cnt >= 4:    # 치즈 토핑이 네 개 이상이면 콰트로치즈피자
-    print('yummy')
-else:
-    print('sad')
+print('yummy') if cnt >= 4 else print('sad')      # 치즈 토핑이 네 개 이상인지 판별
+
         
         
