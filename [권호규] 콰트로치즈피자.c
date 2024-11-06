3a245eea07f0a7d0ad12455f101e035bb34d764f
kwon59 <gregorio3201@naver.com>
2024-11-06 17:31:01 +0900

    [Boj] 콰트로치즈피자

--- [권호규] 콰트로치즈피자.c
+++ [권호규] 콰트로치즈피자.c
@@ -3,6 +3,8 @@
 #include <stdbool.h>
 
 int main(void) {
+
+
     int number;
     scanf("%d", &number);
     char topping[101][101];
@@ -11,44 +13,46 @@ int main(void) {
         scanf("%s", topping[i]);
     }
 
-    char ch[10] = "Cheese";
+    char cheese[10] = "Cheese";
     int cnt = 0;
-    bool inspect = true;
 
     char cheeseTopping[101][101];
 
     for (int i = 0; i < number; i++) {
-        inspect = true;
+        bool inspect = true;
 
+        int toplen = strlen(topping[i]);
         // 문자열이 "Cheese"로 끝나는지 확인
         for (int j = 0; j < 6; j++) {
-            if (topping[i][strlen(topping[i]) - 6 + j] != ch[j]) {
+            if (topping[i][toplen - 6 + j] != cheese[j]) {
                 inspect = false;
             }
         }
 
         // 중복 확인
-        bool Duplicate = false;
+        bool checkDuplicate = false;
         if (inspect) {
             for (int k = 0; k < cnt; k++) {
                 if (strcmp(cheeseTopping[k], topping[i]) == 0) {
-                    Duplicate = true;
+                    checkDuplicate = true;
                     break;
                 }
             }
 
             // 중복이 아닌 경우에만 추가
-            if (!Duplicate) {
+            if (!checkDuplicate) {
                 strcpy(cheeseTopping[cnt], topping[i]);
                 cnt++;
             }
         }
     }
+
     if(cnt >= 4) {
         printf("yummy");
+        return 0;
     }
-    else {
-        printf("sad");
-    }
+
+    printf("sad");
+
     return 0;
 }
