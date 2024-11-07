d24be6c74a850dff368750ee498d5fca492af74b
SangJunny <lsj010115@gmail.com>
2024-11-07 14:45:23 +0900

    Update [이상준] 콰트로치즈피자.java

--- [이상준] 콰트로치즈피자.java
+++ [이상준] 콰트로치즈피자.java
@@ -13,6 +13,11 @@ public class Main {
 
         for (int i = 0; i < number; i++) {
             String topping = in.readLine();
+            //Cheese 문자열 보다 짧은 문자는 넘어감
+            if(topping.length() < 6){
+                continue;
+            }
+            //Cheese 로 끝나는 문자열 중복없이 셋에 저장
             if (topping.endsWith(CHEESE)) {
                 toppingSet.add(topping);
                 if (toppingSet.size() == 4) {
