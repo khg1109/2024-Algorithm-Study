153606c5e26144e03b45582cbaf0ae8dd9f4c3bf
StrangeJun <lsj010115@gmail.com>
2024-11-28 01:11:15 +0900

    [Boj] 좌표 정렬하기 2 ver.2

--- [이상준]좌표 정렬하기 2.java
+++ [이상준]좌표 정렬하기 2.java
@@ -1,45 +1,39 @@
 import java.io.*;
-import java.util.*;
+import java.util.ArrayList;
 
-public class Main {
+
+public class Boj_11651_2 {
     private static final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     private static final BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
     public static void main(String[] args) throws IOException {
         int n = Integer.parseInt(br.readLine());
 
-        ArrayList<Integer> arrayX = new ArrayList<>();
-        ArrayList<Integer> arrayY = new ArrayList<>();
-        HashSet<Integer> setY = new HashSet<>();
+        ArrayList<int[]> points = new ArrayList<>();
 
-        for (int i = 0; i < n; i++) {
+        for(int i = 0 ; i < n; i++){
             String[] input = br.readLine().split(" ");
             int x = Integer.parseInt(input[0]);
             int y = Integer.parseInt(input[1]);
-
-            arrayX.add(x);
-            arrayY.add(y);
-            setY.add(y);
+            int[] point = {x, y};
+            points.add(point);
         }
 
-        for (int k : setY) {
-            ArrayList<Integer> arrayList = new ArrayList<>();
-
-            for (int i = 0; i < arrayY.size(); i++) {
-                if (k == arrayY.get(i)) {
-                    arrayList.add(arrayX.get(i));
-                }
+        points.sort((a, b) -> {
+            if (a[1] == b[1]){
+                return a[0] - b[0];
+            }else{
+                return a[1] - b[1];
             }
+        });
 
-            Collections.sort(arrayList);
-
-            for (int j : arrayList) {
-                bw.write(j + " " + k + "\n");
-            }
+        for (int[] point : points) {
+            bw.write(point[0] + " " + point[1] + "\n");
         }
 
         bw.flush();
         bw.close();
         br.close();
     }
+
 }
